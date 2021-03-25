7e45070bcaa0d11078f2d3ffebb0ad663e31f6c0
Martin Aeschlimann <martinae@microsoft.com>
2021-03-25 18:05:32 +0100

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+All notable changes to the "ext-mar-2021a" extension will be documented in this file.
+
+Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+
+## [Unreleased]
+
+- Initial release
---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,21 @@
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+// import * as myExtension from '../../extension';
+
+suiteSetup(async () => {
+    const extension: vscode.Extension<any> | undefined = vscode.extensions.getExtension('foo.ext-mar-2021a');
+	assert(extension);
+    assert(extension.isActive);
+});
+
+suite('Extension Test Suite', () => {
+	vscode.window.showInformationMessage('Start all tests.');
+
+	test('Sample test', () => {
+		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	});
+});
